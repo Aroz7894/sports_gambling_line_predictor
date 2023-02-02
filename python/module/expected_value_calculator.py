f4551422c456fe59b54de408988d92d409f89bfb
Aroz7894 <andrew.rozniakowski@gmail.com>
2023-02-01 20:39:10 -0500

    setup git ignore and finished tests

--- python/module/expected_value_calculator.py
+++ python/module/expected_value_calculator.py
@@ -3,11 +3,11 @@
 # The implied win percentage can be from a sports betting model or calculated using the no vig “fair” odds from a sharp sportsbook.
 # Finding the no vig “fair” odds from the sharpest sportsbook in the world (you know what it is!) is the industry standard for fair win probability.
 def calculate_expected_value(fair_win_prob: float, stake: float, odds: float):
-    if odds > 100:
+    if abs(odds) > 100:
         odds = convert_amer_odds_to_decimal(odds)
     ev = (fair_win_prob * profit_calculator(odds, stake)) - \
         ((1-fair_win_prob)*stake)
-    return ev
+    return round(ev, 2)
 
 
 def profit_calculator(odds: float, stake: float):
@@ -20,4 +20,4 @@ def convert_amer_odds_to_decimal(amer_odds: int):
         decimal_odds = amer_odds/100 + 1
     elif amer_odds < 0:
         decimal_odds = 100/abs(amer_odds) + 1
-    return decimal_odds
+    return round(decimal_odds, 5)
--- tests/test_expected_value_calculator.py
+++ tests/test_expected_value_calculator.py
@@ -1,10 +1,10 @@
 import pytest
-from module.expected_value_calculator import convert_amer_odds_to_decimal, calculate_expected_value, profit_calculator
+from python.module.expected_value_calculator import convert_amer_odds_to_decimal, calculate_expected_value, profit_calculator
 
 
 def test_convert_amer_odds_to_decimal():
     amer_odds = convert_amer_odds_to_decimal(-150)
-    assert amer_odds == 1.667
+    assert amer_odds == 1.66667
     amer_odds = convert_amer_odds_to_decimal(110)
     assert amer_odds == 2.1
     amer_odds = convert_amer_odds_to_decimal(0)
@@ -16,3 +16,12 @@ def test_profit_calculator():
     assert profit == 110
     profit = profit_calculator(7.0, 100)
     assert profit == 600
+
+
+def test_calculate_expected_value():
+    ev = calculate_expected_value(.5, 100, 110)
+    assert ev == 5
+    ev = calculate_expected_value(.65, 100, -300)
+    assert ev == -13.33
+    ev = calculate_expected_value(.25, 250, 1000)
+    assert ev == 437.50
